54f898df29d98ebfb332a381ca60950b42ea54df
sujoysenmyself <sujoysenjitu1994@gmail.com>
2024-06-01 16:47:10 +0530

    contact form working

---
+++ assets/js/data.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwUjRTFZqzHDVfYkhdGN3e7VIiK3OWkCRhCIfCLCb0eEAWHO1FlGcNgavDtkf_Vi_INUg/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
